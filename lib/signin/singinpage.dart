bbe762b7056dbb020b19a9569229114ca7a936eb
ngtinhs <ngtinh@gmail.com>
2023-03-04 11:37:14 +0700

    abc

--- lib/signin/singinpage.dart
+++ lib/signin/singinpage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,3 +8,14 @@ import 'package:appth1/signup/signupsreen.dart';
 // import 'package:fluttertoast/fluttertoast.dart';
 
 import '../components/body.dart';
+
+class singinpage extends StatelessWidget {
+  static String routeName = "/sign_in";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Body(),
+    );
+  }
+}
